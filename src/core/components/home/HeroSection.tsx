531b3ca64c378ea339c4b093024fd15b40e71366
AdmeBenAbdallah <adembenabdallah.contact@gmail.com>
2024-07-25 16:47:57 +0100

    push

--- src/core/components/home/HeroSection.tsx
+++ src/core/components/home/HeroSection.tsx
@@ -2,8 +2,18 @@ import classes from "@/styles/module/HeroContentLeft.module.css";
 import { Routes } from "@blitzjs/next";
 import { Button, Container, Overlay, Text, Title } from "@mantine/core";
 import Link from "next/link";
+import { useEffect } from "react";
 
 const HeroSection = () => {
+  useEffect(() => {
+    const elements = document.querySelectorAll(".fade-up-stagger > div");
+    elements.forEach((el, index) => {
+      setTimeout(() => {
+        el.classList.add("show");
+      }, index * 150);
+    });
+  }, []);
+
   return (
     <div className={classes.hero}>
       <Overlay
@@ -12,23 +22,31 @@ const HeroSection = () => {
         zIndex={0}
       />
       <Container className={classes.container} size="md">
-        <Title className={classes.title}>Une pièce sans livres est comme un corps sans âme.</Title>
-        <Text className={classes.description} size="xl" mt="xl">
-          Découvrez notre vaste collection de livres de différents genres et auteurs. Commencez votre voyage littéraire
-          avec nous dès aujourd'hui !
-        </Text>
+        <div className="fade-up-stagger">
+          <div className="hidden">
+            <Title className={`${classes.title}`}>Une pièce sans livres est comme un corps sans âme.</Title>
+          </div>
+          <div className="hidden">
+            <Text className={` ${classes.description}`} size="xl" mt="xl">
+              Découvrez notre vaste collection de livres de différents genres et auteurs. Commencez votre voyage
+              littéraire avec nous dès aujourd'hui !
+            </Text>
+          </div>
 
-        <Button
-          variant="gradient"
-          gradient={{ from: "rgba(171, 93, 10, 1)", to: "rgba(196, 159, 118, 1)", deg: 90 }}
-          size="xl"
-          radius="xl"
-          className={classes.control}
-          component={Link}
-          href={Routes.ProductsPage()}
-        >
-          Commencer
-        </Button>
+          <div className="hidden">
+            <Button
+              variant="gradient"
+              gradient={{ from: "rgba(171, 93, 10, 1)", to: "rgba(196, 159, 118, 1)", deg: 90 }}
+              size="xl"
+              radius="xl"
+              className={classes.control}
+              component={Link}
+              href={Routes.ProductsPage()}
+            >
+              Commencer
+            </Button>
+          </div>
+        </div>
       </Container>
     </div>
   );
